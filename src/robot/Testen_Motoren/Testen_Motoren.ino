68117d0727b7fdb7618fffc0a74ce5f005432207
Hector <hector.brasfalean@student.ap.be>
2019-02-22 10:12:33 +0100

    Update Testen_Motoren.ino
    Methods added to drive forward,backwards, turn 90 degrees left and right

--- src/robot/Testen_Motoren/Testen_Motoren.ino
+++ src/robot/Testen_Motoren/Testen_Motoren.ino
@@ -1,26 +1,46 @@
-Pin1 = A1;
-Pin2 = A2;
-Pin3 = A3;
-Pin4 = A4;
+int In1 = A1; // Stuurt moter A aan
+int In2 = A2;
+int In3 = A3;
+int In4 = A4;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(Pin1,OUTPUT);
-  pinMode(Pin2,OUTPUT);
-  pinMode(Pin3,OUTPUT);
-  pinMode(Pin4,OUTPUT);
+  pinMode(In1, OUTPUT);
+  pinMode(In2, OUTPUT);
+  pinMode(In3, OUTPUT);
+  pinMode(In4, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(Pin1,HIGH);
-  digitalWrite(Pin2,LOW);
-  digitalWrite(Pin3,LOW);
-  digitalWrite(Pin4,HIGH);
-  delay(5000);
-  digitalWrite(Pin1,LOW);
-  digitalWrite(Pin2,HIGH);
-  digitalWrite(Pin3,HIGH);
-  digitalWrite(Pin4,LOW);
-  delay(5000);
+  DriveForward(5000);
+  Turn90DegreesLeft(2000);
+}
+void Turn90DegreesLeft(int timeDriving) {
+  digitalWrite(In1, HIGH);
+  digitalWrite(In2, HIGH);
+  digitalWrite(In3, LOW);
+  digitalWrite(In4, HIGH);
+  delay(timeDriving);
+}
+void Turn90DegreesRight(int timeDriving) {
+ digitalWrite(In1, HIGH);
+  digitalWrite(In2, LOW);
+  digitalWrite(In3, HIGH);
+  digitalWrite(In4, HIGH);
+  delay(timeDriving);
+}
+void DriveForward(int timeDriving) {
+  digitalWrite(In1, HIGH);
+  digitalWrite(In2, LOW);
+  digitalWrite(In3, LOW);
+  digitalWrite(In4, HIGH);
+  delay(timeDriving);
+}
+void DriveBackwards(int timeDriving) {
+  digitalWrite(In1, LOW);
+  digitalWrite(In2, HIGH);
+  digitalWrite(In3, HIGH);
+  digitalWrite(In4, LOW);
+  delay(timeDriving);
 }
