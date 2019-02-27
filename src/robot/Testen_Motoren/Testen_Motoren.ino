1f18d669a845e85ae4618e691c8c94e8aa4bf146
Hector <hector.brasfalean@student.ap.be>
2019-02-27 15:58:07 +0100

    Robot code has changed
    Arduino code for robot has changed :
    - Robot can turn 90 degrees to left/right with a time of 2103 ms.

--- src/robot/Testen_Motoren/Testen_Motoren.ino
+++ src/robot/Testen_Motoren/Testen_Motoren.ino
@@ -5,6 +5,7 @@ int In4 = A4;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(In1, OUTPUT);
   pinMode(In2, OUTPUT);
   pinMode(In3, OUTPUT);
@@ -13,21 +14,25 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  DriveForward(5000);
-  Turn90DegreesLeft(2000);
+  DriveForward(5000);         // 5 seconden naar voor rijden
+  Turn90DegreesLeft(2103);     // 2103/90 = 23,367 ms per °
+  Turn90DegreesRight(2103);   
+  DriveBackwards(5000);         // 5 seconden naar achter rijden
 }
-void Turn90DegreesLeft(int timeDriving) {
-  digitalWrite(In1, HIGH);
+void Turn90DegreesRight(int timeDriving) {
+  digitalWrite(In1, LOW);
   digitalWrite(In2, HIGH);
   digitalWrite(In3, LOW);
   digitalWrite(In4, HIGH);
+  Serial.println("Turning Left");
   delay(timeDriving);
 }
-void Turn90DegreesRight(int timeDriving) {
+void Turn90DegreesLeft(int timeDriving) {
  digitalWrite(In1, HIGH);
   digitalWrite(In2, LOW);
   digitalWrite(In3, HIGH);
-  digitalWrite(In4, HIGH);
+  digitalWrite(In4, LOW);
+  Serial.println("Turning Right");
   delay(timeDriving);
 }
 void DriveForward(int timeDriving) {
@@ -35,6 +40,7 @@ void DriveForward(int timeDriving) {
   digitalWrite(In2, LOW);
   digitalWrite(In3, LOW);
   digitalWrite(In4, HIGH);
+  Serial.println("Driving forward");
   delay(timeDriving);
 }
 void DriveBackwards(int timeDriving) {
@@ -42,5 +48,6 @@ void DriveBackwards(int timeDriving) {
   digitalWrite(In2, HIGH);
   digitalWrite(In3, HIGH);
   digitalWrite(In4, LOW);
+  Serial.println("Driving backwards");
   delay(timeDriving);
 }
