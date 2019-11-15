373a344b60f731410da37501197253a5768d5c1b
Jens Peeters <jenspeeters99@hotmail.com>
2019-11-15 12:06:41 +0100

    Clean-up server

---
+++ src/Server/Models/Measurement.cs
@@ -0,0 +1,11 @@
+﻿namespace Server.Models
+{
+    public class Measurement
+    {
+        public long Id { get; set; }
+        public string Mac_Anchor { get; set; }
+        public string Mac_Tag { get; set; }
+        public double Distance { get; set; }
+        public string Unix_Timestamp { get; set; }
+    }
+}
